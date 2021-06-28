cfd67304116b7c57bac650734661893639aa83de
Bruno <bruno_silth@hotmail.com>
2021-06-28 17:52:16 -0300

    criacao nova classe Tema, mudanças em postagem para fazer relacionamento

--- src/main/java/org/generation/blogPessoalTeste/controller/PostagemController.java
+++ src/main/java/org/generation/blogPessoalTeste/controller/PostagemController.java
@@ -22,115 +22,102 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/postagem")
-@CrossOrigin ("*")
+@CrossOrigin("*")
 public class PostagemController {
 
 	@Autowired
 	private PostagemRepository repository;
-	
-	// METODOS QUE IMPLEMENTEI POR BUSCA
-	
-	@GetMapping ("/all")
-	public List<Postagem> findAll(){
+
+	// METODOS-TESTE QUE IMPLEMENTEI POR BUSCA
+
+	@GetMapping("/all")
+	public List<Postagem> findAll() {
 		return repository.findAll();
 	}
-	
-	@GetMapping ("/Id")
-	public Optional<Postagem> findById(){
+
+	@GetMapping("/Id")
+	public Optional<Postagem> findById() {
 		return repository.findById((long) 3);
 	}
-	
-	@GetMapping ("/titulo")
-	public List<Postagem> getByTitulo () {
+
+	@GetMapping("/titulo")
+	public List<Postagem> getByTitulo() {
 		return repository.getByTituloContainingIgnoreCase("Cores");
 	}
 
-	//METODOS PELO PROFESSOR NO VIDEO
-	
-    @GetMapping ("/GetAll") //método findAll
-    public  ResponseEntity<List<Postagem>> getAll() {
-        return ResponseEntity.ok(repository.findAll());
-    }
-	
-    @GetMapping ("/GetAllById/{id}") //método findByID
-    public  ResponseEntity<Postagem> getAllById(@PathVariable long id) {
-        return repository.findById(id)
-        		.map(resp -> ResponseEntity.ok(resp)).
-        		orElse(ResponseEntity.notFound().build());
-    }
-	
-    @GetMapping ("/GetByTitulo/{titulo}") //método getByTitulo
-    public  ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
-        return ResponseEntity.ok(repository.getByTituloContainingIgnoreCase(titulo));
-    }
-    
-    @PostMapping //método POST
-    public ResponseEntity<Postagem> post(@RequestBody Postagem postagem) {
-    	return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));    
-    }
-    
-    @PutMapping //método PUT
-    public ResponseEntity<Postagem> put(@RequestBody Postagem postagem) {
-    	return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));    
-    }
-    
-    @DeleteMapping ("/{id}") //método DELETE
-    public  void delete (@PathVariable long id) {
-       repository.deleteById(id);
-    }
-	
-    
-    
-    
-	@GetMapping ("/getByIdteste")
-	public Postagem findByIdteste(){
-		return repository.getById((long) 3);
-		
+	// METODOS PELO PROFESSOR NO VIDEO
+
+	@GetMapping("/GetAll") // método findAll
+	public ResponseEntity<List<Postagem>> findAllPostagem() {
+		return ResponseEntity.ok(repository.findAll());
 	}
-		
-	@GetMapping ("/getById/{id_usuario}")
-	public ResponseEntity<Postagem> batata(@PathVariable(value = "id_usuario") Long id) throws EntityNotFoundException{
-		Optional<Postagem> objeto = Optional.ofNullable(repository.getById(id));
+
+	@GetMapping("/GetAllById/{id}") // método findByID
+	public ResponseEntity<Postagem> findByIdPostagem(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/GetByTitulo/{titulo}") // método getByTitulo
+	public ResponseEntity<List<Postagem>> findByDescricaoTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(repository.getByTituloContainingIgnoreCase(titulo));
+	}
+
+	@PostMapping // método POST
+	public ResponseEntity<Postagem> postPostagem(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+
+	@PutMapping // método PUT
+	public ResponseEntity<Postagem> putPostagem(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+
+	@DeleteMapping("/{id}") // método DELETE
+	public void deletePostagem(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+
+	// MÉTODOS-TESTE TENTANDO USAR GETBYID
+
+	@GetMapping("/getByIdteste")
+	public Postagem findByIdteste() {
+		return repository.getById((long) 3);
+
+	}
+
+	@GetMapping("/getById/{id_usuario}")
+	public ResponseEntity<Postagem> batata(@PathVariable(value = "id_usuario") Long id) throws EntityNotFoundException {
+		Optional<Postagem> objeto = (repository.findById(id)); // Optional.ofNullable(repository.getById(id));
+																// --anterior que nao funcionava
 		if (objeto.isPresent()) {
-			Postagem nova  = objeto.get();
+			Postagem nova = objeto.get();
 			System.out.println(objeto.toString());
 			return ResponseEntity.status(200).body(nova);
 		} else {
 			return ResponseEntity.status(400).build();
 		}
 	}
-	
-	
 
-	
 	/*
-	@GetMapping ("/Id")
-	public Postagem encontraPorId(){
-		return repository.getById((long) 3);
-	}
-	*/
-	
-	
+	 * @GetMapping ("/Id") public Postagem encontraPorId(){ return
+	 * repository.getById((long) 3); }
+	 */
+
 	/*
-	@GetMapping ("/Id")
-	public List<Postagem> findAllById(){
-		return repository.findAllById(long id);
-	}
-	*/
-	
-	/*@GetMapping ("/teste")
-	public List<Postagem> getById(){
-		return repository.getById(1);
-	}
-	*/
-	
-	
-	
-	/*public void findAll(PostagemRepository repository) {
-        this.repository = repository;
-    }
+	 * @GetMapping ("/Id") public List<Postagem> findAllById(){ return
+	 * repository.findAllById(long id); }
+	 */
 
+	/*
+	 * @GetMapping ("/teste") public List<Postagem> getById(){ return
+	 * repository.getById(1); }
+	 */
 
-*/
-	
+	/*
+	 * public void findAll(PostagemRepository repository) { this.repository =
+	 * repository; }
+	 * 
+	 * 
+	 */
+
 }
