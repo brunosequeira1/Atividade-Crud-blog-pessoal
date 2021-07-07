1a4cd1cceba2c4ff4069bd3eade15c62fad9c841
Bruno <bruno_silth@hotmail.com>
2021-07-07 20:16:32 -0300

    inserindo segurança

---
+++ src/main/java/org/generation/blogPessoalTeste/controller/UsuarioController.java
@@ -0,0 +1,72 @@
+package org.generation.blogPessoalTeste.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.generation.blogPessoalTeste.model.Usuario;
+import org.generation.blogPessoalTeste.model.dtos.UsuarioDTO;
+import org.generation.blogPessoalTeste.model.dtos.UsuarioLoginDTO;
+import org.generation.blogPessoalTeste.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/v1/usuario")
+public class UsuarioController {
+
+	
+	private @Autowired UsuarioService service;
+	
+
+	@PostMapping ("/salvar")
+	public ResponseEntity<Object> salvar(@Valid @RequestBody Usuario novoUsuario) {
+
+		Optional<Object> cadastrarUsuario = service.cadastrarUsuario(novoUsuario);
+
+		if (cadastrarUsuario.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existente!");
+
+		} else {
+			return ResponseEntity.status(HttpStatus.CREATED).body("Usuário cadastrado.");
+
+		}
+
+	}
+
+	@PutMapping("/credenciais")
+	public ResponseEntity<?> autorizar(@Valid @RequestBody UsuarioLoginDTO dadosParaLogar){
+		return service.pegarCredenciais(dadosParaLogar)
+				.map(usuarioCredenciado -> ResponseEntity.ok(usuarioCredenciado))
+				.orElse(ResponseEntity.badRequest().build());
+		
+	}
+	
+	@PutMapping("/{id}/atualizar")
+	public ResponseEntity<Usuario> atualizarUsuario(@Valid @PathVariable(value = "idUsuario") Long idUsuario,
+			@Valid @RequestBody UsuarioLoginDTO usuarioParaAtualizar) {
+
+		return service.atualizarUsuario(idUsuario, usuarioParaAtualizar)
+				.map(usuarioAtualizado -> ResponseEntity.ok().body(usuarioAtualizado))
+				.orElse(ResponseEntity.badRequest().build());
+
+	}
+
+
+
+	
+}
---
+++ src/main/java/org/generation/blogPessoalTeste/model/dtos/UsuarioDTO.java
@@ -0,0 +1,8 @@
+package org.generation.blogPessoalTeste.model.dtos;
+
+public class UsuarioDTO {
+
+	
+	
+	
+}
